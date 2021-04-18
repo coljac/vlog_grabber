30d64b870fa6b8361ee5cd005c37c736bcaebcc2
Colin Jacobs <colin@coljac.net>
2021-04-18 14:32:38 +1000

    Initial commit. It works.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# vlog_grabber
-Discord bot to grab .vlog files
+# Vlog grabber
+
+This is a simple Discord bot to grab file attachments from the specified channels and save them to the filesystem.
+
+Usage: 
+Set the token in an environment variable, `DISCORD_TOKEN`.
+
+```python vlog_grabber_bot.py channel1[,channel2,...] /path/to/save```
---
+++ vlog_grabber_bot.py
@@ -0,0 +1,58 @@
+import discord
+import os
+import sys
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+options = {"notify": False}
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.channel.name in options['channels']:
+        if len(message.attachments) > 0:
+            for a in message.attachments:
+                if a.filename.endswith(".vlog"):
+                    try:
+                        await a.save(f"/tmp/{a.filename}")
+                        if options['notify']:
+                            await message.channel.send(f"Got {a.filename}.")
+                    except Exception as e:
+                        print(repr(e))
+
+
+def printhelp():
+    print("vlog_grabber_bot [-notify] <channel1[,channel2,...]> </path/to/save>")
+
+
+def main():
+    if len(sys.argv) < 2:
+        printhelp()
+        return
+
+    if "-notify" in sys.argv:
+        options['notify'] = True
+        sys.argv.remove('-notify')
+
+    options['channels'] = sys.argv[1].split(",")
+    try:
+        options['savedir'] = sys.argv[2]
+    except:
+        options['savedir'] = "."
+
+    if not os.path.exists(options['savedir']):
+        print(f"Error: Save path {options['savedir']} does not exist.")
+        return
+
+    client.run(TOKEN)
+
+if __name__ == "__main__":
+    main()
